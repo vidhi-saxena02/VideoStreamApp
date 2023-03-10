00aea582a8d2f3f6cb28652d06c44dced0533454
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-11 00:38:36 +0530

    video Schema added

---
+++ server/model/video.schema.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const VideoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please enter a title"],
+    maxLength: [100, "Title cannot exceed 100 characters"],
+    minLength: [3, "Title must be at least 3 characters"],
+  },
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: [true, "Please enter a description"],
+    maxLength: [1000, "Description cannot exceed 1000 characters"],
+    minLength: [3, "Description must be at least 3 characters"],
+  },
+
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  dislikes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Video = mongoose.model("Video", VideoSchema);
+module.exports = Video;
