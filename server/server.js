ea794b385ce69e210838f248eda48e845805001f
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-11 02:27:00 +0530

    error handler added

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const http = require("http");
-const app = require("./app");
 const dotenv = require("dotenv");
-const ConnectDB = require("./mongo/database");
-
+const connectDB = require("./mongo/database");
+const app = require("./app");
 const server = http.createServer(app);
 
 dotenv.config({ path: "./config/config.env" });
 
-ConnectDB();
+connectDB();
 
+//Unhandle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
 server.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(`Listening on PORT ${process.env.PORT}`);
+});
+
+//Unhandle promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down the server due to unhandled promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
