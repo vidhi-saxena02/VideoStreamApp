9b8eca050526051515a41b98023ac4d9aba1d198
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-11 02:37:45 +0530

    middleware added

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const UserRouter = require("./routes/user.route");
 const bodyParser = require("body-parser");
+const ErrorMiddleware = require("./middleware/error");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/v1", UserRouter);
+app.use(ErrorMiddleware);
 
 module.exports = app;
