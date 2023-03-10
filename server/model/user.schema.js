860937b8abe1be145173ce8045d8abec0f28d258
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-11 00:19:20 +0530

    User schema created

---
+++ server/model/user.schema.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    maxLength: [20, "Your name cannot exceed 20 characters"],
+    minLength: [3, "Your name must be at least 3 characters"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [6, "Minimum password length is 6 characters"],
+    select: false,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
