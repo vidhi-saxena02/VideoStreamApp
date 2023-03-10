d73ebe05235f9974be682b438086de389942bf42
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-11 02:28:10 +0530

    export modified

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const UserRouter = require("./routes/user.route");
+const bodyParser = require("body-parser");
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/v1", UserRouter);
 
-exports.app = app;
+module.exports = app;
