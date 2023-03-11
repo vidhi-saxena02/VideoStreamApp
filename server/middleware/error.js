cbd0a178b80357f18eb13663c6d8347704d8ce47
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-11 14:44:53 +0530

    mongoose error added

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -9,6 +9,25 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  //Mongoose Duplicate Key Error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new Errorhandler(message, 400);
+  }
+
+  //JWt Error
+
+  if (err.name === "JsonWebTokenError") {
+    const message = "Json Web Token is invalid. Try Again!!!";
+    err = new Errorhandler(message, 400);
+  }
+
+  //JWT Expired Error
+  if (err.name === "TokenExpiredError") {
+    const message = "Json Web Token is expired. Try Again!!!";
+    err = new Errorhandler(message, 400);
+  }
+
   return res.status(err.statusCode).json({
     success: false,
     message: err.message,
