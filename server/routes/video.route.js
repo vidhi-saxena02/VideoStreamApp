2e56363c0788d0062764e14e446b649435cbc95d
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-12 13:55:07 +0530

    like/dislike video route added

--- server/routes/video.route.js
+++ server/routes/video.route.js
@@ -6,6 +6,8 @@ const {
   updateVideo,
   deleteVideo,
   addView,
+  likeVideo,
+  dislikeVideo,
 } = require("../controllers/video.controller");
 
 const { isAuthenticatedUser } = require("../middleware/auth");
@@ -17,6 +19,9 @@ VideoRouter.route("/video/:id")
   .get(getSingleVideo)
   .put(isAuthenticatedUser, updateVideo)
   .delete(isAuthenticatedUser, deleteVideo);
+
+VideoRouter.route("/video/like/:id").put(isAuthenticatedUser, likeVideo);
+VideoRouter.route("/video/dislike/:id").put(isAuthenticatedUser, dislikeVideo);
 
 VideoRouter.route("/video/view/:id").put(addView);
 
