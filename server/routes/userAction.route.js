45e269d038501a9c77cd74d0d98abe083492f6f5
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-12 13:54:31 +0530

    like/dislike route removed

--- server/routes/userAction.route.js
+++ server/routes/userAction.route.js
@@ -3,17 +3,10 @@ const express = require("express");
 const UserActionRouter = express.Router();
 const { isAuthenticatedUser } = require("../middleware/auth");
 const {
-  likeComment,
-  dislikeComment,
   commentVideo,
   replyComment,
   getAllComments,
 } = require("../controllers/userAction.controller");
-
-// like and dislike a video
-UserActionRouter.route("/comment/:id")
-  .put(isAuthenticatedUser, likeComment)
-  .put(isAuthenticatedUser, dislikeComment);
 
 //comment on a video
 UserActionRouter.route("/video/comment/:id")
